9c8f0bcde029a980804c9945a17ea03c74661670
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 21:32:42 +0300

    Add HomeDto

---
+++ Task10.Core/DTOs/HomeDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task10.Test.Core.Models;
+
+namespace Task10.Core.DTOs
+{
+    public sealed class HomeDto
+    {
+        public IEnumerable<Course> Courses { get; set; }
+
+        public IEnumerable<Group> Groups { get; set; }
+
+        public IEnumerable<Student> Students { get; set; }
+    }
+}
