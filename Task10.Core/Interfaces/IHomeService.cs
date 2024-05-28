07d022fdceefdfdbe07af917af42bc7201fc99e8
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 21:34:58 +0300

    Add GetGomeDto method in IHomeService

--- Task10.Core/Interfaces/IHomeService.cs
+++ Task10.Core/Interfaces/IHomeService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task10.Core.DTOs;
 
 namespace Task10.Core.Interfaces
 {
     public interface IHomeService
     {
+        public HomeDto GetHomeDto();
     }
 }
