0377d7e82a5f9105753b83596c6034ad6a8853b3
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 21:05:32 +0300

    Add Students repo

---
+++ Task10/Repos/StudentsRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task10.Test.Core.Interfaces;
+using Task10.Test.Core.Models;
+
+namespace Task10.Infrastructure.Repos
+{
+    public sealed class StudentsRepository(CoursesDbContext dbContext) : IStudentsRepository
+    {
+        private readonly CoursesDbContext _dbContext = dbContext;
+
+        public async Task AddAsync(Student entity)
+        {
+            await _dbContext.Students.AddAsync(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Student entity)
+        {
+            _dbContext.Students.Remove(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Student> GetByIdAsync(int id)
+        {
+            return await _dbContext.Students.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Student>> GetAllAsync()
+        {
+            return await _dbContext.Students.ToListAsync();
+        }
+
+        public async Task UpdateAsync(Student entity)
+        {
+            _dbContext.Students.Update(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
