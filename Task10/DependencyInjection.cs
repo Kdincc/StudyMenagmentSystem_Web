0252de210a80538bb4313b42a59ceec3b58219e1
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 21:06:55 +0300

    Add repos to DI

--- Task10/DependencyInjection.cs
+++ Task10/DependencyInjection.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Task10.Infrastructure.Repos;
+using Task10.Test.Core.Interfaces;
 
 namespace Task10.Infrastructure
 {
@@ -18,6 +20,10 @@ namespace Task10.Infrastructure
             {
                 options.UseSqlServer(configuration.GetConnectionString("DbString"));
             });
+
+            services.AddScoped<ICoursesRepository, CourseRepository>();
+            services.AddScoped<IGroupsRepository, GroupsRepository>();
+            services.AddScoped<IStudentsRepository, StudentsRepository>();
 
             return services;
         }
