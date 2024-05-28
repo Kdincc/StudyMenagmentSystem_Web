943a8dd022dcd4c53f462682f40106dc74119727
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 20:54:22 +0300

    Add repos interfaces

---
+++ Task10.Core/Interfaces/ICoursesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task10.Core.Interfaces;
+using Task10.Test.Core.Models;
+
+namespace Task10.Test.Core.Interfaces
+{
+    public interface ICoursesRepository : IRepository<Course>
+    {
+    }
+}
