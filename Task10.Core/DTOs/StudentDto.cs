ead02bbf7a8c5b1cd1a7cc89abc9752b2f9fbee5
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 21:25:54 +0300

    Add base Dtos

---
+++ Task10.Core/DTOs/StudentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10.Core.DTOs
+{
+    public sealed class StudentDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int GroupId { get; set; }
+    }
+}
