99dd4587b29a5ceb1ea64ec7b097458c90bf5103
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 21:02:16 +0300

    Add CourseRepository

---
+++ Task10/Repos/CourseRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task10.Test.Core.Interfaces;
+using Task10.Test.Core.Models;
+
+namespace Task10.Infrastructure.Repos
+{
+    public sealed class CourseRepository(CoursesDbContext dbContext) : ICoursesRepository
+    {
+        private readonly CoursesDbContext _dbContext = dbContext;
+
+        public async Task AddAsync(Course entity)
+        {
+            await _dbContext.Courses.AddAsync(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Course entity)
+        {
+            _dbContext.Courses.Remove(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Course>> GetAllAsync()
+        {
+            return await _dbContext.Courses.ToListAsync();
+        }
+
+        public async Task<Course> GetByIdAsync(int id)
+        {
+            return await _dbContext.Courses.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Course entity)
+        {
+            _dbContext.Courses.Update(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
