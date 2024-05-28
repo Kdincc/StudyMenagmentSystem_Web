81792fae57efe489e9e87f1459ec1befcf38ddcf
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 22:22:11 +0300

    Add IGroupService

---
+++ Task10.Core/Interfaces/IGroupService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task10.Core.DTOs;
+
+namespace Task10.Core.Interfaces
+{
+    public interface IGroupService
+    {
+        public Task<IEnumerable<GroupDto>> GetGroupsAsync();
+
+        public Task CreateGroupAsync(string groupName, int courseId);
+
+        public Task DeleteGroupAsync(int groupId);
+
+        public Task EditGroupAsync(GroupDto groupDto);
+    }
+}
