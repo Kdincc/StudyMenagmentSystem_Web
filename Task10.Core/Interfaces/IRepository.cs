9775ff5d37640cd2da64b4d92fdd044e91f52dd5
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 20:52:51 +0300

    Add IRepository Interface

---
+++ Task10.Core/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task10.Test.Core.Models;
+
+namespace Task10.Core.Interfaces
+{
+    public interface IRepository<T> where T : DbEntity
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+
+        public Task<T> GetByIdAsync(int id);
+
+        public Task AddAsync(T entity);
+
+        public Task UpdateAsync(T entity);
+
+        public Task DeleteAsync(T entity);
+    }
+}
