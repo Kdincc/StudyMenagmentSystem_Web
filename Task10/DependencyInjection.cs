83c0a492fe03b7d7adb591cf211f834f631df93e
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-05-28 21:08:21 +0300

    Clean usings

--- Task10/DependencyInjection.cs
+++ Task10/DependencyInjection.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using Task10.Infrastructure.Repos;
 using Task10.Test.Core.Interfaces;
 
@@ -16,7 +10,7 @@ namespace Task10.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<CoursesDbContext>(options => 
+            services.AddDbContext<CoursesDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("DbString"));
             });
